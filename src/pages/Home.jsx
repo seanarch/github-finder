a8c7de33c060bb1f4db16771216dbad8e17ae42c
Sean <seanzhu@Seans-Mac-mini.local>
2022-07-10 23:23:51 -0600

    Get search result on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
- 
+import UserResults from "../components/users/UserResults"
+
 export default function Home() {
   return (
-    <div>
-        <h1 className="text-6xl">Welcome</h1>
-        {process.env.REACT_APP_GITHUB_TOKEN} 
-    </div>
+    <>
+        <UserResults /> 
+    </>
   )
 }
