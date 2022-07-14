f9e04e315443aa9f1a9517c9ffe6f15efbad003d
Sean <seanzhu@Seans-Mac-mini.local>
2022-07-13 21:11:21 -0600

    Update search function

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -17,20 +17,24 @@ export const GithubProvider = ({children}) => {
 
 
     // Get initial users (testing purpose)
-    const fetchUsers = async () => {
+    const searchUsers = async (text) => {
         setLoading()
 
-        const response = await fetch(`${GITHUB_URL}/users`, {
+        const params = new URLSearchParams({
+            q: text
+        })
+
+        const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
             headers: {
                 Authorization: `token ${GITHUB_TOKEN}`,
             }
         })
 
-        const data = await response.json()
+        const {items} = await response.json()
 
         dispatch({
             type: 'GET_USERS',
-            payload: data,
+            payload: items,
         })
 
  
@@ -41,7 +45,7 @@ export const GithubProvider = ({children}) => {
     return <GithubContext.Provider value={{
         users: state.users,
         loading: state.loading,
-        fetchUsers
+        searchUsers
 
     }}>
         {children}
